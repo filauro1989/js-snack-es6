6cd2a62262d62b1f768c0767bcb08067524a2eca
filauro1989 <filauro1989@gmail.com>
2021-12-14 10:55:32 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// Scrivere una funzione (e lanciarla :razzo:) che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+// La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+
+let dio = [
+    2000,
+    20,
+    43,
+    8000,
+    57,
+    46,
+    17,
+    28,
+    89,
+    110
+];
+
+
+
+function snack(array, numero1, numero2) {
+    let pippo = array.filter((element, index) => {
+        return index > numero1 && index < numero2;
+    });
+    return pippo
+}
+
+let maiale = snack(dio, 0, 5);
+
+
+console.log(maiale);
